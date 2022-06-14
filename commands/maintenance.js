730819ad8e61b556838be29d623667d0ec90ec96
MentoskaPraha <filipjanstary@gmail.com>
2022-06-14 13:54:09 +0200

    Reworked Back-End

--- commands/maintenance.js
+++ commands/maintenance.js
@@ -1,6 +1,8 @@
 //libraries
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { maintianerId } = require('../config.json');
+const { MessageActionRow, MessageButton } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const { maintianerId, verificationEmbedColor } = require('../config.json');
 
 //command information
 module.exports = {
@@ -15,7 +17,16 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand.setName('terminate')
                 .setDescription('Terminates the bot.')
-            ),
+            )
+		.addSubcommand(subcommand =>
+			subcommand.setName('verification')
+				.setDescription('Creates the verification message in the selected channel.')
+				.addChannelOption(option =>
+					option.setName('channel')
+						.setDescription('The channel where the verification will take place.')
+						.setRequired(true)
+					)
+			),
 	
 	//run this code if the command is called
 	async execute(interaction) {
@@ -32,12 +43,45 @@ module.exports = {
                 //terminate the bot
         	    process.exit(0);  
             }
+
+			//if the subcommand is verification run the following
+			if (interaction.options.getSubcommand() === 'verification'){
+				//create the message
+				const embed = new MessageEmbed()
+					.setColor(verificationEmbedColor)
+					.setTitle('Join the Big Chungus Religion!')
+					.setDescription(`By clicking the button to below and joining the religion you agree to all of our rules. If you break these rules the council ha the right to banish you from the religion.`)
+					.setThumbnail(interaction.guild.iconURL())
+
+				const row = new MessageActionRow()
+					.addComponents(
+						new MessageButton()
+							.setCustomId('join')
+							.setStyle('SUCCESS')
+							.setLabel('Join')
+					)
+				
+				//get the channel
+				const channel = interaction.options.getChannel('channel');
+
+				//send it to the channel
+				channel.send({ embeds: [embed], components: [row] });
+
+				//log it to the console
+				console.log(`${interaction.user.tag} created a new verification message.`);
+
+				//respond to the user that action was successful
+				await interaction.reply({content: 'Verification message created successfully!', ephemeral: true});
+
+				//prevent unecessery if statements
+				return;
+			}
 		} else{
 			//respond if error message if user does not have termination permission
 			await interaction.reply({content: 'You do not have permissions to run this command.', ephemeral: true});
             
             //log action
-			console.log(`${interaction.user.tag} attempted to access maintinence commands.`);
+			console.log(`${interaction.user.tag} attempted to access maintenance commands.`);
 		}
 	},
 };
