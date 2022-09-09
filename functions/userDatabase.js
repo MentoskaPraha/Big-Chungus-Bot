51e5d7da63bdbd30832d0ea1e6e880f4fdd2f6f1
MentoskaPraha <filipjanstary@gmail.com>
2022-09-09 14:10:52 +0300

    Prepared for DB testing (mising entry delete)

--- functions/userDatabase.js
+++ functions/userDatabase.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 
 //database
-const db = new Sequelize('users', 'admin', 'wordpass123', {
+const db = new Sequelize('users', 'admin', 'wordpass12', {
     host: 'localhost',
     dialect: 'sqlite',
     loggong: 'false',
@@ -16,8 +16,14 @@ const dbEntry = db.define('users', {
         unique: true,
         defaultVaule: 0
     },
-    title: Sequelize.STRING,
-    birthday: Sequelize.DATEONLY,
+    title: {
+        type: Sequelize.STRING,
+        defaultValue: "",
+    },
+    birthday: {
+        type: Sequelize.DATEONLY,
+        defaultValue: 0
+    },
     PersonalVcId: {
         type: Sequelize.INTEGER,
         unique: true,
@@ -29,6 +35,57 @@ const dbEntry = db.define('users', {
 module.exports = {
     async initUserDb(){
         await dbEntry.sync();
-    }
+    },
 
+    addEntry(id, title, birthday, PersonalVcId){
+        //create a new entry
+        try {
+            const entry = dbEntry.new({
+                id: id,
+                title: title,
+                birthday: birthday,
+                PersonalVcId: PersonalVcId 
+            });
+
+            return 0;
+        } catch (error) {
+            if (error.name === 'SequelizeUniqueConstraintError') {
+				return 1;
+			}
+
+			return 2;
+        }
+    },
+
+    async getEntry(id){
+        //get a vailid tag
+        const entry = await dbEntry.findOne({where: {id: id}});
+
+        if(!entry) return null;
+
+        return entry;
+    },
+
+    async editEntry(id, newTitle, newBirthday, newPersonalVcId){
+        //edit a tag
+        var editedTags = 0;
+
+        if(newTitle != null){
+            editedTags += dbEntry.update({ title: newTitle }, { where: { id: id } });
+        }
+
+        if(newBirthday != null){
+            editedTags += dbEntry.update({ title: newTitle }, { where: { id: id } });
+        }
+
+        if(newPersonalVcId != null){
+            editedTags += dbEntry.update({ title: newTitle }, { where: { id: id } });
+        }
+
+        if(!editedTags >= 1){
+            return 1;
+        }
+
+        return 0;
+    }
 }
--- index.js
+++ index.js
@@ -66,7 +66,6 @@ for (const file of functionFiles) {
 
 //let the user know that the bot is almost ready
 console.log('Preforming final preparations...');
-client.functions.userDatabase
 
 // Login to Discord with your client's token
 console.log('Logging in...');
